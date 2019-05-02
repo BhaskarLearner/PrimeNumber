a10de7bdc57f8ada7b86b98087f75fe3ca7f9fbc
Bhaskar <bhaskarlearner@gmail.com>
2019-05-02 04:10:07 -0700

    Second Commit

--- src/main/java/a/b/c/App.java
+++ src/main/java/a/b/c/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
     }
     public boolean isPrime(int n){
         for(int i=2;i<n/2;i++){
