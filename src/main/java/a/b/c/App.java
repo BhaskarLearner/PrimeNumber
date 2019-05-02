8f98736ff30162f831b922383971ac6d873cc70b
Bhaskar <bhaskarlearner@gmail.com>
2019-05-02 04:32:46 -0700

    Thrid Commit

--- src/main/java/a/b/c/App.java
+++ src/main/java/a/b/c/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-System.out.println( "Hello World!" );
+System.out.println( "Hello!" );
     }
     public boolean isPrime(int n){
         for(int i=2;i<n/2;i++){
