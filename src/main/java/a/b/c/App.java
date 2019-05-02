4861ec8bba8d77741d7bc6c33097cfaac84c8207
Bhaskar <bhaskarlearner@gmail.com>
2019-05-02 03:44:42 -0700

    First Commit
    First Commit

---
+++ src/main/java/a/b/c/App.java
@@ -0,0 +1,19 @@
+package a.b.c;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public boolean isPrime(int n){
+        for(int i=2;i<n/2;i++){
+		if(n%i==0) return false;
+	}
+	return true;
+    }
+}
